c5b8de180dc163619534272642a482eac5b12a2b
kutub <350emon@gmail.com>
2018-10-18 23:52:15 +0600

    add postgresql database

--- src/guide/settings.py
+++ src/guide/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'guide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'preparation'),
+        'USER': os.environ.get('DB_USER', 'preparation_guide'),
+        'PASSWORD': os.environ.get('DB_PASS', 'Sokran350'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
